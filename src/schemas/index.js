253dbdd7fd102cbce5cc4e295c4e6a5b4a8e609f
Pushpam Priya <pushpam.priyaa@gmail.com>
2024-03-10 23:21:55 +0530

    deployed website

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -8,7 +8,7 @@ const lastnameRegex = /^[A-Z][a-zA-Z]*\s*$/;
 export const signupSchema = Yup.object({
     firstName:Yup.string().matches(firstnameRegex, 'First letter shoud be capital').min(2,'First Name must be atleast 2 characters long').max(25,'First Name cannot be more than 25 characters').required('*Required'),
     lastName:Yup.string().matches(lastnameRegex, 'Last name should be of one word with first letter capital').min(2,'Last Name must be atleast 2 characters long').max(25,'Last Name cannot be more than 25 characters').required('*Required'),
-    address:Yup.string().min(10,'Address must be atleast 10 characters long').required('This field is required'),
+    address:Yup.string().min(10,'Address must be atleast 10 characters long').required('*Required'),
     email: Yup.string().matches(emailRegex,'Invalid email id').max(64,'Email cannot be more than 64 characters').email('Invalid email address').required('*Required'),
     pass1:Yup.string().matches(passwordRegex, 'Password must contain at least one lowercase letter, one uppercase letter, one digit, one special character (@, $, !, %, *, ?, &), and be a minimum of 8 characters long.').required('*Required'),
     pass2: Yup.string().required('*Required').oneOf([Yup.ref('pass1'),null],'Password must match'),
